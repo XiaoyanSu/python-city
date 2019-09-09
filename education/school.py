4de715b5f32f3d9a9712a540f22769fdd9ab5522
Xiaoyan Su <xiaoyansu@dhcp-wifi-8021x-155-41-75-95.bu.edu>
2019-09-09 17:50:55 -0400

    day 1

--- education/school.py
+++ education/school.py
@@ -1,3 +1,17 @@
-def draw_school():
-    print("school not found")
+def draw_school(floors=0):
+    """prints a school with specified # of floors
+        Args:
+            floors: integer representing the number of floors above ground level
+    """
+    #Sanitize Parameter
+    if floors < 0:
+        raise ValueError("number of floors cannot be negative")
+    #Printing School
+    print("  .  ")
+    print(" / \\ ")
+    print(" | | ")
+    print("/   \\")
+    for _ in range(floors):
+        print("|   |")
+    print("|___|")
     return
